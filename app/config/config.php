e5fe9b7692906ab9b64ea946330e9d41e4dd1b28
Mba Thomas <angelfab631@gmail.com>
2024-02-11 11:31:11 +0300

    Create config.php

---
+++ app/config/config.php
@@ -0,0 +1,21 @@
+<?php
+// DB config
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'maxcourier');
+
+// Root
+define('APPROOT', dirname(dirname(__FILE__)));
+// Url Root
+define('URLROOT', 'http://localhost/courier-redesign');
+// Site Name
+define('SITENAME', 'JETKLIN LOGISTICS');
+define('EMAIL', 'jetklin-logistics@mail.com');
+define('WEBSITE', 'jetklinlogistics.com');
+define('PHONE', '+15645246902');
+// Site Logo
+define('SITELOGO', '<h5 class="text-uppercase mb-0" style="color: white;">-FreeTown&acd;</h5>
+<h5 class="text-uppercase mb-0 text-white"> &ThinSpace; &ThinSpace; &ThinSpace; Hotel&Suit-</h5>');
+
+// die(APPROOT);
