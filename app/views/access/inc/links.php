5b7a53c6b6f5db0157dbf283241a516274683343
Mba Thomas <angelfab631@gmail.com>
2024-02-12 21:47:15 +0300

    Create links.php

---
+++ app/views/access/inc/links.php
@@ -0,0 +1,25 @@
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="<?= URLROOT ?>/admin-assets/css/bootstrap-5.2.3.min.css">
+<!-- FONT AWESOME -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+<!-- LINE AWESOME -->
+<link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
+<!-- CUSTOM CSS -->
+<link rel="stylesheet" href="<?= URLROOT ?>/admin-assets/css/dashboard.css" />
+<link rel="stylesheet" href="<?= URLROOT ?>/admin-assets/css/print.css" media="print">
+<!-- FAVICON -->
+<link rel="shortcut icon" type="image/x-icon" href="<?= URLROOT ?>/admin-assets/img/logo/logo.png">
+<!-- GOOGLE FONTS LINK -->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Urbanist:wght@400;500;600;700&display=swap" rel="stylesheet">
+
+<link href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css" rel="stylesheet" />
+<link href="https://cdn.datatables.net/colreorder/1.6.2/css/colReorder.dataTables.min.css" rel="stylesheet" />
+<link href="https://cdn.datatables.net/responsive/2.4.1/css/responsive.dataTables.min.css" rel="stylesheet" />
+<link href="https://cdn.datatables.net/rowreorder/1.3.3/css/rowReorder.dataTables.min.css" rel="stylesheet" />
+<link href="https://cdn.datatables.net/scroller/2.1.1/css/scroller.dataTables.min.css" rel="stylesheet" />
+<link href="https://cdn.datatables.net/searchpanes/2.1.2/css/searchPanes.dataTables.min.css" rel="stylesheet" />
+
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flat-icons@1.0.0/creative.min.css">
+<link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
