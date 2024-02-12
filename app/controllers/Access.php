24ca6b15366d5c273fff331157b6cbef81dc0870
Mba Thomas <angelfab631@gmail.com>
2024-02-12 21:46:24 +0300

    Update Access.php

--- app/controllers/Access.php
+++ app/controllers/Access.php
@@ -318,6 +318,7 @@ class Access extends Controller
          </div>
       </div>
    </div>";
+   
       $headers = "MIME-Version: 1.0" . "\r\n";
       $headers .= "Content-Type:text/html;charset=UTF-8" . "\r\n";
 
