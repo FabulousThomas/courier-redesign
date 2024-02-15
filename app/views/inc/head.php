d2ec8c88606e85d0d303ab616cfa39cf80f2a575
Mba Thomas <angelfab631@gmail.com>
2024-02-15 11:07:02 +0300

    Create head.php

---
+++ app/views/inc/head.php
@@ -0,0 +1,99 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="keywords" content="courier Logistics Shipping Transport Air Land ">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="<?= URLROOT ?>/assets/css/bootstrap-5.2.3.min.css">
+    <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flat-icons@1.0.0/creative.min.css">
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <link rel="stylesheet" href="<?= URLROOT ?>/assets/css/style.css" />
+
+    <link rel="shortcut icon" type="image/x-icon" href="<?= URLROOT ?>/assets/img/logo/logo.png">
+    <title><?= SITENAME; ?></title>
+</head>
+
+<body class="bg-white content">
+
+    <!-- <style>
+        #loader {
+            position: absolute;
+            top: 0;
+            bottom: 0;
+            left: 0;
+            right: 0;
+            margin: auto;
+        }
+
+        @keyframes spin {
+            100% {
+                transform: rotate(360deg);
+            }
+        }
+
+
+        .spinner {
+            border: 5px solid #f3f3f3;
+            border-radius: 50%;
+            border-top: 5px solid #f15f22;
+            border-right: 5px solid #f3f3f3;
+            border-bottom: 5px solid #f15f22;
+            width: 80px;
+            height: 80px;
+            -webkit-animation: spin 2s linear infinite;
+            animation: spin 2s linear infinite;
+        }
+
+        @-webkit-keyframes spin {
+            0% {
+                -webkit-transform: rotate(0deg);
+            }
+
+            100% {
+                -webkit-transform: rotate(360deg);
+            }
+        }
+
+        @keyframes spin {
+            0% {
+                transform: rotate(0deg);
+            }
+
+            100% {
+                transform: rotate(360deg);
+            }
+        }
+    </style>
+     -->
+    <!-- <div class="d-flex align-items-center justify-content-center" id="loader">
+        <div class="spinner text-white">
+            <span class="visually-hidden">Loading...</span>
+        </div>
+    </div> -->
+    <!-- <script>
+        document.onreadystatechange = function() {
+            if (document.readyState !== "complete") {
+                document.querySelector(
+                    "body").style.visibility = "hidden";
+                document.querySelector(
+                    "#loader").style.visibility = "visible";
+            } else {
+                document.querySelector(
+                    "#loader").style.display = "none";
+                document.querySelector(
+                    "body").style.visibility = "visible";
+            }
+        };
+    </script> -->
+
+
+    <!--CSS Spinner-->
+    <!-- <div class="spinner-wrapper d-flex align-items-center justify-content-center" id="loader">
+        <div class="spinner text-white" role="status">
+            <span class="visually-hidden">Loading...</span>
+        </div>
+    </div> -->
