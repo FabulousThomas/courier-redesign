62840a329c8ade6472af4d88b4c1d86edb802b7d
Mba Thomas <angelfab631@gmail.com>
2024-02-15 11:06:57 +0300

    Create footer.php

---
+++ app/views/inc/footer.php
@@ -0,0 +1,114 @@
+<footer class="black-bg py-5">
+   <div class="container">
+      <div class="row m-0">
+         <div class=" mb-4">
+            <h2 class="text-white">We Understand The Importance Approaching Each Work!</h2>
+            <a href="mailto:<?= EMAIL ?>"><?= EMAIL ?> </a>
+         </div>
+         <hr>
+         <div class="col-lg-4 col-md-6 mb-4">
+            <div class="footer-tittle">
+               <h4 class="text-white">COMPANY</h4>
+               <ul class="footer-nav">
+                  <a href="mailto:<?= EMAIL ?>"></a>
+                  <li class="nav-item"><a class="nav-link text-white-50" href="#about">About Us</a></li>
+                  <li class="nav-item"><a class="nav-link text-white-50" href="#company">Company</a></li>
+               </ul>
+            </div>
+         </div>
+         <div class="col-lg-4 col-md-6 mb-4">
+            <div class="footer-tittle">
+               <h4 class="text-white">OPEN HOUR</h4>
+               <ul class="footer-nav">
+                  <li class="nav-item"><a class="nav-link text-white-50" href="#">Monday 8am-8pm</a></li>
+                  <li class="nav-item"><a class="nav-link text-white-50" href="#">Tuesday-Friday 7am-8pm</a></li>
+                  <li class="nav-item"><a class="nav-link text-white-50" href="#">Saturday 10am-8pm</a></li>
+                  <li class="nav-item"><a class="nav-link text-white-50" href="#">Sunday 11am-8pm</a></li>
+               </ul>
+            </div>
+         </div>
+         <div class="col-lg-4 col-md-6 mb-4">
+            <div class="footer-logo mb-3">
+               <a href="<?= URLROOT ?>"><img src="<?= URLROOT ?>/assets/img/logo/logo.png" width="100px" alt="LOGO"></a>
+            </div>
+            <div class="footer-tittle">
+               <div class="footer-pera">
+                  <small class="text-white-50">$10 BILLION NET WORTH, 500 EMPLOYEES, 30 BRANCHES YOU CAN TRUST US AS
+                     YOUR NUMBER 1 LOGISTICS COMPANY. CORE VALUE IS OUR FUNDAMENTAL BELIEF.</small>
+               </div>
+            </div>
+
+            <div class="footer-social ">
+               <a href="#"><i class="text-white-50 lab la-facebook-f"></i></a>
+               <a href="#"><i class="text-white-50 lab la-twitter"></i></a>
+               <a href="#"><i class="text-white-50 lab la-instagram"></i></a>
+               <a href="#"><i class="text-white-50 las la-globe"></i></a>
+            </div>
+         </div>
+      </div>
+
+      <div class="footer-bottom mt-5">
+         <div class="row d-flex align-items-center">
+            <div class="col-lg-12">
+               <div class="footer-copy-right text-center">
+                  <small>
+                     Copyright &copy; <script>
+                        document.write(new Date().getFullYear());
+                     </script> | All rights reserved
+                  </small>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+
+</footer>
+
+
+<!-- JS -->
+<script src="<?= URLROOT ?>/assets/js/jquery-3.6.1.min.js"></script>
+<script src="<?= URLROOT ?>/assets/js/bootstrap-5.2.3.bundle.min.js"></script>
+<!-- SLICK JS -->
+<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+
+<script src="<?= URLROOT ?>/assets/js/slick.js"></script>
+<script src="<?= URLROOT ?>/assets/js/script.js"></script>
+
+<script>
+   // PAGE PRELOADER
+   // document.onreadystatechange = function() {
+   //     if (document.readyState !== "complete") {
+   //         document.querySelector("body").style.visibility = "hidden";
+   //         document.querySelector("#loader").style.visibility = "visible";
+   //     } else {
+   //         document.querySelector("#loader").style.display = "none";
+   //         document.querySelector("body").style.visibility = "visible";
+   //     }
+   // };
+
+   //  AOS ANIMATION
+   AOS.init();
+</script>
+
+
+
+<!-- <small style="font-size: .8rem;">Translate this page:</small> -->
+<!-- <div id="google_translate_element">
+</div> -->
+
+<script type="text/javascript">
+   function googleTranslateElementInit() {
+      new google.translate.TranslateElement({
+         pageLanguage: 'en'
+      }, 'google_translate_element');
+   }
+</script>
+
+<script src="<?= URLROOT ?>/assets/js/element.js"></script>
+
+<!-- <small style="font-size: .8rem;">You can translate the content of this page by selecting a language in the select box.</small> -->
+
+</body>
+
+</html>
