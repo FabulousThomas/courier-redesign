ea9ddc502d885ed435853d1ab71dad92662be19f
Mba Thomas <angelfab631@gmail.com>
2024-02-17 15:21:59 +0300

    Create slick.js

---
+++ public/assets/js/slick.js
@@ -0,0 +1,63 @@
+$('.slider-wrapper').slick({
+   slidesToShow: 1,
+   slidesToScroll: 1,
+   autoplay: true,
+   autoplaySpeed: 4000,
+   nextArrow: $('.next'),
+   prevArrow: $('.prev'),
+
+   responsive: [{
+      breakpoint: 1024,
+      settings: {
+         slidesToShow: 1,
+         slidesToScroll: 1,
+         infinite: true,
+         // dots: true
+      }
+   },
+   {
+      breakpoint: 760,
+      settings: {
+         slidesToShow: 1,
+         slidesToScroll: 1
+      }
+   },
+   {
+      breakpoint: 511,
+      settings: {
+         slidesToShow: 1,
+         slidesToScroll: 1
+      }
+   }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+   ]
+});
+
+// ClassicEditor.create(document.querySelector('#body'), {
+//    toolbar: ['heading', '|', 'bold', 'italic', 'link', 'bulletedList', 'numberedList', 'blockQuote'],
+//    heading: {
+//       options: [{
+//          model: 'paragraph',
+//          title: 'Paragraph',
+//          class: 'ck-heading_paragraph'
+//       },
+//       {
+//          model: 'heading1',
+//          view: 'h1',
+//          title: 'Heading 1',
+//          class: 'ck-heading_heading1'
+//       },
+//       {
+//          model: 'heading2',
+//          view: 'h2',
+//          title: 'Heading 2',
+//          class: 'ck-heading_heading2'
+//       }
+//       ]
+//    }
+// })
+//    .catch(error => {
+//       console.log(error);
+//    });
